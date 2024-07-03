771bc901b29e43c5c039c5221a106ea59190370d
Geronimo-montes <geronimomontes@gmail.com>
2024-07-03 12:31:33 -0700

    Auto-generated commit

--- src/app/@theme/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './hero-card/hero-card.component';
-export * from './hero-list-card/hero-list-card.component';
-export * from './pagination/pagination.component';
-export * from './hero-detail/hero-detail.component';
-export * from './comic-list/comic-list.component';
-export * from './notification/notification.component';
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://gateway.marvel.com/v1/public',
-  publicApiKey: '2907c8ec9241c7acb000267798df758f',
-  privateApiKey: '17d2049f26ee6937110d0cbc353de13b28202cd5',
-}
